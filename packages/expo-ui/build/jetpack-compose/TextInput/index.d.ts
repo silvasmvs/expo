2e837c6c1a54bb9e025006962c47bee995efba8d
joaomoreno99 <joao.moreno.ext@e-ai.com.br>
2025-07-30 16:43:49 -0300

    sdk-53

--- packages/expo-ui/build/jetpack-compose/TextInput/index.d.ts
+++ packages/expo-ui/build/jetpack-compose/TextInput/index.d.ts
@@ -1,30 +1,51 @@
-import { Ref } from 'react';
-import { StyleProp, ViewStyle } from 'react-native';
-import { ExpoModifier, ViewEvent } from '../../types';
+import React from 'react';
+import { ViewEvent } from '../../types';
+import { StyleProp, ViewStyle } from "react-native";
 /**
  * @hidden Not used anywhere yet.
  */
 export type TextInputRole = 'default' | 'cancel' | 'destructive';
-export type TextInputRef = {
-    setText: (newText: string) => Promise<void>;
-};
+interface TextStyleProps {
+    color?: string;
+    size?: number;
+    lineHeight?: number;
+    letterSpacing?: number;
+    height?: number;
+    fontFamily?: string;
+    fontWeight?: string;
+}
 export type TextInputProps = {
-    /**
-     * Can be used for imperatively setting text on the TextInput component.
-     */
-    ref?: Ref<TextInputRef>;
     /**
      * Additional styles to apply to the TextInput.
      */
     style?: StyleProp<ViewStyle>;
     /**
+     * Additional styles to apply to the TextInput.
+     */
+    textStyle?: TextStyleProps;
+    /**
      * Initial value that the TextInput displays when being mounted. As the TextInput is an uncontrolled component, change the key prop if you need to change the text value.
      */
     defaultValue?: string;
     /**
+     * The string that will be rendered before text input has been entered.
+     */
+    placeholder?: string;
+    /**
      * A callback triggered when user types in text into the TextInput.
      */
-    onChangeText: (value: string) => void;
+    onChangeText?: (value: string) => void;
+    /**
+     * A callback triggered when user focus TextInput.
+     */
+    onTextFieldFocus?: () => void;
+    /**
+     * A callback triggered when user blur TextInput.
+     */
+    onTextFieldBlur?: () => void;
+    /**
+     * The string that will be rendered before text input has been entered.
+     */
     /**
      * If true, the text input can be multiple lines.
      * While the content will wrap, there's no keyboard button to insert a new line.
@@ -59,20 +80,28 @@ export type TextInputProps = {
      */
     autocorrection?: boolean;
     /**
-     * Options to request software keyboard to capitalize the text. Applies to languages which has upper-case and lower-case letters.
-     *
-     * Available options:
-     * - `characters`: Capitalize all characters.
-     * - `none`: Do not auto-capitalize text.
-     * - `sentences`: Capitalize the first character of each sentence.
-     * - `unspecified`: Capitalization behavior is not specified.
-     * - `words`: Capitalize the first character of every word.
-     * @default none
-     * @platform android
+     * If true, the text input is editable.
      */
-    autoCapitalize?: 'characters' | 'none' | 'sentences' | 'unspecified' | 'words';
-    /** Modifiers for the component */
-    modifiers?: ExpoModifier[];
+    editable?: boolean;
+    /**
+     * The string with the testId for E2E tests.
+     */
+    testID?: string;
+    /**
+     * If true, password text field will be showed.
+     */
+    secureEntry?: boolean;
+    /**
+     * The mask that should be aplied to the field. Please follow the following pattern:
+     * [0]: mandatory digit. For instance, [000] will allow entering three digits: 123.
+       [9]: optional digit.For instance, [00099] will allow entering up to five digits, but at least three.
+       [A]: mandatory letter. For instance, [AAA] will allow entering three letters: ABC.
+       [a]: optional letter. [АААааа] will allow entering from three to six letters.
+       [_]: mandatory symbol (digit or letter).
+       [-]: optional symbol (digit or letter).
+       […]: ellipsis. Allows to enter endless count of symbols.
+     */
+    mask?: string;
 };
 export type NativeTextInputProps = Omit<TextInputProps, 'onChangeText'> & {} & ViewEvent<'onValueChanged', {
     value: string;
@@ -80,5 +109,6 @@ export type NativeTextInputProps = Omit<TextInputProps, 'onChangeText'> & {} & V
 /**
  * Renders a `TextInput` component.
  */
-export declare function TextInput(props: TextInputProps): import("react").JSX.Element;
+export declare function TextInput(props: TextInputProps): React.JSX.Element;
+export {};
 //# sourceMappingURL=index.d.ts.map
